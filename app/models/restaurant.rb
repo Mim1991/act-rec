3a7d5e4567370afd661619edf8d3042c4e488a98
Meehir Patel <meehir_patel@hotmail.co.uk>
2022-10-31 11:07:17 +0000

    upload files

---
+++ app/models/restaurant.rb
@@ -0,0 +1,32 @@
+class Restaurant < ActiveRecord::Base
+#  CRUD
+# # Create
+#   restaurant = Restaurant.new(name: "dishoom")
+#   restaurant.save
+
+#   # Read
+#   # Read all
+#   Restaurant.all
+#   # Read one
+#   Restaurant.find(1)
+
+#   # Update
+#   rest = Restaurant.find(1)
+#   rest.address = "London"
+#   rest.save
+
+#   # Destroy
+#   rest = Restaurant.find(1)
+#   rest.destroy
+
+#   # Filtering
+#   Restaurant.where(rating: 5)
+#   Restaurant.where('name LIKE ?', "%pizza%")
+
+#   # filter for one item
+#   Restaurant.find_by(name: "Dishoom")
+
+
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,30 @@
 # This is where you can create initial data for your app.
+# puts "Creating restaurants!"
+# restaurant = Restaurant.new(name: "Five Guys", rating: 4, address: "London")
+# restaurant.save
+# puts "Created Restaurant 1"
+# restaurant2 = Restaurant.new(name: "Sticks and Sushi", rating: 5, address: "London")
+# restaurant2.save
+# puts "Created Restaurant 2"
+# puts "All done!"
+
+require 'faker'
+
+# puts "Creating Restaurants!"
+# 100.times do |t|
+#   restaurant = Restaurant.new(name: Faker::Restaurant.name, rating: rand(0..5), address: Faker::Address.street_name)
+#   restaurant.save
+#   puts "Created Restaurant #{t}"
+# end
+# puts "All done!"
+
+require 'json'
+require 'rest-client'
+
+response = RestClient.get "https://api.github.com/users/lewagon/repos"
+data = JSON.parse(response)
+
+data.each do |repo|
+  restaurant = Restaurant.new(name: repo["name"], rating: rand(0..5), address: Faker::Address.street_name)
+  restaurant.save
+end
